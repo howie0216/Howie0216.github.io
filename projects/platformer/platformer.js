53407f87077be20103b3268a9f498436bffaa592
howie0216 <howard.martin@sciacademy.org>
2024-09-18 14:51:02 +0000

    message

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -61,9 +61,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-    createCannon("left", 600,700); 
+    createCannon("left", 600,900); 
     createCannon("bottom", 500, 900);
-    createCannon("right", 150, 300);
+    createCannon("right", 150, 900);
     createCannon("top", 800, 700);
 
     
